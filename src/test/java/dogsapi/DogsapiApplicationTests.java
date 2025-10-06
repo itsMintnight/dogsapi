e547ecb08d1d6f9db7c2c112fa5dfce69fafa4a4
itsMintnight <sagezhang01@gmail.com>
2025-10-06 13:44:21 -0400

    intial commit

---
+++ src/test/java/dogsapi/DogsapiApplicationTests.java
@@ -0,0 +1,13 @@
+package dogsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogsapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
