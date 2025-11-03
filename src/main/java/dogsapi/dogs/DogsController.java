077adfc4fdead555c9d45a25ce81861386640471
itsMintnight <sagezhang01@gmail.com>
2025-11-03 15:35:09 -0500

    add ability to view dogs and create dogs

--- src/main/java/dogsapi/dogs/DogsController.java
+++ src/main/java/dogsapi/dogs/DogsController.java
@@ -3,6 +3,8 @@ package dogsapi.dogs;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,9 +12,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
-@RestController
+@Controller
 public class DogsController {
     @Autowired
     private DogsService dogsService;
@@ -21,14 +23,37 @@ public class DogsController {
      * Get Mappings
      */
     @GetMapping("/dogs")
-    public List<Dogs> getAllDogs() {
-        return dogsService.getAllDogs();
+    public String getAllDogs(Model model) {
+        // return dogsService.getAllDogs();
+        model.addAttribute("allDogs", dogsService.getAllDogs());
+        return "dogs-list";
     }
 
     @GetMapping("/dogs/{dogID}")
-    public Dogs getDogByID(@PathVariable Long dogID) {
-        return dogsService.getDogByID(dogID);
+    public String getDogByID(Model model, @PathVariable Long dogID) {
+        // return dogsService.getDogByID(dogID);
+        model.addAttribute("dog", dogsService.getDogByID(dogID));
+        return "dogs-details";
     }
+
+    @GetMapping("/dogs/createForm")
+    public String showCreateForm(Model model) {
+        Dogs dog = new Dogs();
+        model.addAttribute(dog);
+        return "dogs-create";
+    }
+
+    @PostMapping("/dogs")
+    public String createDog(Dogs dog) {
+        dogsService.createDog(dog);
+        Long dogID = dog.getDogID();
+        return "redirect:/dogs/" + dogID;
+    }
+
+  
+
+
+
 
     @GetMapping("/dogs/breed/{breed}") 
     public List<Dogs> getDogsByBreed(@PathVariable String breed) {
@@ -40,13 +65,7 @@ public class DogsController {
         return dogsService.getDogsByName(name);
     }
 
-    /*
-     * Post Mappings
-     */
-    @PostMapping("/dogs")
-    public Dogs createDog(@RequestBody Dogs dog) {
-        return dogsService.createDog(dog);
-    }
+ 
 
     /*
      * Put Mappings
